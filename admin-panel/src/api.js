e23aed9589478e884172bb522fbea61bf923daf4
M Saad Mustafa <msaadmustafa091@gmail.com>
2026-02-25 06:21:13 -0800

    axios error solve

--- admin-panel/src/api.js
+++ admin-panel/src/api.js
@@ -7,11 +7,10 @@ import axios from 'axios'
 //     withCredentials:true
 // });
 const api = axios.create({
-<<<<<<< HEAD
 
-=======
+
+
   baseURL:"https://mern-ecommerce-app-ncuh.onrender.com/user",
->>>>>>> 7a362e2 (Initial commit)
   withCredentials:true
 });
 
